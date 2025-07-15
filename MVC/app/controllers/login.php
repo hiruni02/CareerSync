e153d9e3be8e83e6677c0a66033d65ebf6834aab
Anuk <anuk.thotawatta@gmail.com>
2025-07-15 23:46:18 +0530

    added placeholder login page with CSS

---
+++ MVC/app/controllers/login.php
@@ -0,0 +1,6 @@
+<?php
+    class login extends Controller{
+        public function index(){
+            $this->view("login");
+        }
+    }
---
+++ MVC/app/views/login.view.php
@@ -0,0 +1,20 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="assets/css/test.css">
+        <title>Login</title>
+    </head>
+    <body>
+        <div class="login-container">
+            <h1>Login</h1>
+            <form method="POST" action="https://www.youtube.com/watch?v=dQw4w9WgXcQ">
+                <input type="email" placeholder="Email" name="email" required>
+                <input type="password" placeholder="Password" name="password" required>
+                <button type="submit">Log In</button>
+            </form>
+            <div class="links">
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Create Account</a></t>
+                <a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ">Forgot password?</a>
+            </div>
+        </div>
+    </body>
+</html>
