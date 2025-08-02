a67442ad5d800e9b11fe909be98e5f92c79d4bfa
Anuk-thotawatta <125145613+Anuk-thotawatta@users.noreply.github.com>
2025-08-03 00:02:13 +0530

    reverted accidental push in footer.php

--- MVC/app/views/footer.php
+++ MVC/app/views/footer.php
@@ -5,32 +5,8 @@
             <h1 class="logo">CareerSync</h1>
             <p>Empowering careers. Connecting talent</p>
         </div>
-        <div class="col">
-            <h4>Useful links:</h4>
-            <ul><li><a href="home">Home</a></li>
-                <li><a href="login">Login</a></li>
-                <li><a href="register">Register</a></li>
-                <li><a href="about">About us</a></li>
-            </ul>
-        </div>
-        <div class="col">
-            <p>
-                <h4>For more information</h4>
-                <h5>Dial:</h5>
-                <h5>E-mail:</h5>
-            </p>
-        </div>
-        <div class="col">
-            <p></p>
-        </div>
-        <div class="col">
-            <p>Terms and conditions</p>
-        </div>
-        <div class="col">
-            <p>Private policy</p>
-        </div>
-        <div class="col">
-            <p><a href="contact">Contact us</a> </p>
-        </div>
+        <div class="col"></div>
+        <div class="col"></div>
+        <div class="col"></div>
     </div>
 </footer>
