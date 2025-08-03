6cd9aa0dd84bd8839aa4ba2adeb03255c1da7bb4
Anuk <anuk.thotawatta@gmail.com>
2025-08-03 10:37:40 +0530

    created a filter section for the jobList

--- MVC/app/views/home.view.php
+++ MVC/app/views/home.view.php
@@ -9,67 +9,70 @@
                     event.preventDefault();
                 }
             }
+
+            window.onload = function () {
+                const rangeInput = document.getElementById("salRange");
+                const valueDisplay = document.getElementById("salValue");
+
+                rangeInput.addEventListener("input", function () {
+                valueDisplay.textContent = Number(this.value).toLocaleString();
+                });
+            };
         </script>
         <title>Home</title>
     </head>
     <body>
-    <?php
-    include("navbar.php");
-    ?>
-    <div class='page-content'>
-        <section class="intro">
-            <div class="intro-content">
-                <h1>Empowering Careers. Connecting Talent.</h1>
-                <p style="padding-bottom: 50px;">One platform for potential employees, companies collaborate.</p>
-                <?php
-                    if($username=='User'){
-                ?>
-                        <p style=" font-size: 20px; padding-bottom:20px;">you are currently exploring as a guest.<br> would you like to:</p>
-                        <div class="intro-buttons">
-                            <a href="login"><button class="intro-btn">Login</button></a>
-                            <p style=" font-size: 25px;">- or -</p>
-                            <a href="welcome"><button class="intro-btn secondary">Register</button></a>
-                        </div>
-                <?php
-                    }else{
-                        ?>
-                            <h2 style="font-family: roboto,sans-serif;">Welcome, <?=$username?></h1>
+        <div class="page-wrapper">
+            <?php
+            include("navbar.php");
+            ?>
+            <div class='page-content'>
+                <section class="intro">
+                    <div class="intro-content">
+                        <h1>Empowering Careers. Connecting Talent.</h1>
+                        <p style="padding-bottom: 50px;">One platform for potential employees, companies collaborate.</p>
                         <?php
-                    }
-                ?>
-                
-            </div>
-        </section>
-        
-        <div class="jobContainer">
-                <div class="scrollBox"><!-- some test values -->
-                    <h3>[ job / internship listing goes here  ]</h3>
-                    <div class="listItem">
-                        job1
+                            if($username=='User'){
+                        ?>
+                                <p style=" font-size: 20px; padding-bottom:20px;">you are currently exploring as a guest.<br> would you like to:</p>
+                                <div class="intro-buttons">
+                                    <a href="login"><button class="intro-btn">Login</button></a>
+                                    <p style=" font-size: 25px;">- or -</p>
+                                    <a href="welcome"><button class="intro-btn secondary">Register</button></a>
+                                </div>
+                        <?php
+                            }else{
+                                ?>
+                                    <h2 style="font-family: roboto,sans-serif;">Welcome, <?=$username?></h1>
+                                <?php
+                            }
+                        ?>
+                        
                     </div>
-                    <div class="listItem">
-                        job2
-                    </div>
-                    <div class="listItem">
-                        job3
-                    </div>
-                    <div class="listItem">
-                        job4
-                    </div>
-                    <div class="listItem">
-                        job5
-                    </div>
-                    <div class="listItem">
-                        job6
-                    </div>
-                    <div class="listItem">
-                        job7
-                    </div>
+                </section>
+                <section class="job-section">
+                <div class="filtersContainer">
+                    <label for="salRange">Salary Range:</label><br>
+                    <input type="range" id="salRange" name="salRange" min="21000" max="10000000" value="21000">
+                    <div id="salValue">21000</div>
                 </div>
+                    <div class="jobContainer">
+                        <div class="scrollBox">
+                            <h3 style="padding-bottom: 25px;">Select a position</h3>
+                            <div class="listItem">job1</div>
+                            <div class="listItem">job2</div>
+                            <div class="listItem">job3</div>
+                            <div class="listItem">job4</div>
+                            <div class="listItem">job5</div>
+                            <div class="listItem">job6</div>
+                            <div class="listItem">job7</div>
+                        </div>
+                    </div>
+                </section>
+            </div>
+            <?php
+            include("footer.php");
+            ?>
         </div>
-    </div>
-    <?php
-    include("footer.php");
-    ?>
     </body>
 </html>
