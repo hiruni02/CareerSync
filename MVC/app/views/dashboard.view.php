1274ab9e0fcf7bc4fff5c9b3c651dee782b5c60d
Saracodes341 <saraniya2424@gmail.com>
2025-08-04 10:36:15 +0530

    updating

---
+++ MVC/app/views/dashboard.view.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<head>
+    <link rel="stylesheet" href="<?=ROOT?>assets/css/common.css">
+    
+    <title>Dashboard</title>
+</head>
+<body>
+    <?php
+    include("navbar.php");
+    ?>
+
+    <h3>This is dashboard</h3>
+    
+</body>
+</html>
