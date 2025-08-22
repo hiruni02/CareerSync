41c6c592cf145448335873c81225dd0cb892f0b2
Anuk <anuk.thotawatta@gmail.com>
2025-08-22 20:45:11 +0530

    created the edit profile popup

--- MVC/app/views/profiles/adminProfile.php
+++ MVC/app/views/profiles/adminProfile.php
@@ -6,12 +6,109 @@
         // show($userTable);
         // show($adminTable);
         ?>
-        <div class="profile_picture"><img src="<?=ROOT.$adminTable->admin_photo_path?>" alt="admin photo"></div>
+        <div class="profile_picture"><img src="<?= ROOT . $adminTable->admin_photo_path ?>" alt="admin photo"></div>
         <div class="info_segment"><label>First name :</label><?php echo $adminTable->firstName; ?></div>
         <div class="info_segment"><label>Last name :</label><?php echo $adminTable->lastName; ?></div>
         <div class="info_segment"><label>Contact number :</label><?php echo $adminTable->contactNo; ?></div>
         <div class="info_segment"><label>email address :</label><?php echo $userTable->email; ?></div>
         <button class="backBtn" id="backBtn">Back</button>
+        <button class="edit_profileBtn" id="editBtn">Edit Profile</button>
+    </div>
+    <div class="edit_profile_display">
+        <div class="editWindow">
+            <h1>Edit profile</h1>
+            <form method="POST" enctype="multipart/form-data">
+                <div class="input-field">
+                    <label for="firstName">First Name</label>
+                    <input
+                        type="text"
+                        placeholder="First Name"
+                        name="firstName"
+                        required
+                        value="<?= $adminTable->firstName ?>">
+                </div>
+
+                <div class="input-field">
+                    <label for="lastName">Last Name</label>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        name="lastName"
+                        required
+                        value="<?= $adminTable->lastName ?>">
+                </div>
+
+                <div class="input-field">
+                    <label for="email">Email Address</label>
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        name="email"
+                        required
+                        value="<?= $userTable->email ?>"
+                        style="<?= !empty($errors['email']) ? 'border: 2px solid red;' : '' ?>">
+                </div>
+                <?php if (!empty($errors['email'])): ?>
+                    <div style="color:red;" class="error"><?= $errors['email'] ?></div>
+                <?php endif; ?>
+
+                <div class="input-field">
+                    <label for="admin_photo_path">Change Profile Picture</label>
+                    <input
+                        type="file"
+                        name="admin_photo_path"
+                        required
+                        accept=".pdf, .jpg, .jpeg, .png"
+                        style="<?= !empty($errors['admin_photo_path']) ? 'border: 2px solid red;' : '' ?>">
+                </div>
+                <?php if (!empty($errors['admin_photo_path'])): ?>
+                    <div style="color:red;" class="error"><?= $errors['admin_photo_path'] ?></div>
+                <?php endif; ?>
+
+                <div class="input-field">
+                    <label for="contactNo">Contact Number</label>
+                    <input
+                        type="tel"
+                        placeholder="Contact Number:07xxxxxxxx"
+                        name="contactNo"
+                        pattern="[0-9]{10}"
+                        required
+                        value="<?= $adminTable->contactNo ?>">
+                </div>
+
+                <div class="input-field">
+                    <label for="password">Password</label>
+                    <input
+                        type="password"
+                        id="pass"
+                        placeholder="Password"
+                        name="password"
+                        required>
+                    <button onclick="show_password()" class="eye" type="button" id="eye1"></button>
+                </div>
+
+                <div class="input-field">
+                    <label for="confirm_password">Re-enter Pasword</label>
+                    <input
+                        type="password"
+                        id="confirm_pass"
+                        placeholder="Confirm Password"
+                        name="confirm_password"
+                        required
+                        style="<?= !empty($errors['confirm_password']) ? 'border: 2px solid red;' : '' ?>">
+                    <button onclick="show_confirm_password()" class="eye" type="button" id="eye2"></button>
+                </div>
+                <?php if (!empty($errors['confirm_password'])): ?>
+                    <div style="color:red; padding-bottom:15px;" class="error"><?= $errors['confirm_password'] ?></div>
+                <?php endif; ?>
+
+                <div class="form_btns">
+                    <button type="submit">Save Changes</button>
+                    <button id="edit_backBtn">Back</button>
+                </div>
+            </form>
+
+        </div>
     </div>
 </div>
 
@@ -20,14 +117,52 @@
         const profileBtn = document.getElementById("profileBtn");
         const backBtn = document.getElementById("backBtn");
         const profileDisplay = document.querySelector(".profile_display");
+        const editBtn = document.getElementById("editBtn");
+        const edit_backBtn = document.getElementById("edit_backBtn");
+        const editProfile = document.querySelector(".edit_profile_display");
 
         profileBtn.addEventListener("click", (e) => {
             e.preventDefault();
-            profileDisplay.classList.toggle("active");
+            profileDisplay.classList.add("active");
         });
 
         backBtn.addEventListener("click", () => {
-            profileDisplay.classList.toggle("active");
+            profileDisplay.classList.remove("active");
+        });
+
+        editBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            editProfile.classList.add("active");
+        });
+
+        edit_backBtn.addEventListener("click", () => {
+            editProfile.classList.remove("active");
         });
     });
+
+    function show_password() {
+        console.log(document.getElementById("pass").type);
+        var x = document.getElementById("pass");
+        if (x.type === "password") {
+            x.type = "text";
+            document.getElementById("eye1").style.backgroundImage = "url(<?= ROOT ?>assets/svg_icons/eye_close.svg)";
+        } else {
+            x.type = "password";
+            document.getElementById("eye1").style.backgroundImage = "url(<?= ROOT ?>assets/svg_icons/eye_open.svg)";
+
+        }
+    }
+
+    function show_confirm_password() {
+        console.log(document.getElementById("confirm_pass").type);
+        var x = document.getElementById("confirm_pass");
+        if (x.type === "password") {
+            x.type = "text";
+            document.getElementById("eye2").style.backgroundImage = "url(<?= ROOT ?>assets/svg_icons/eye_close.svg)";
+        } else {
+            x.type = "password";
+            document.getElementById("eye2").style.backgroundImage = "url(<?= ROOT ?>assets/svg_icons/eye_open.svg)";
+
+        }
+    }
 </script>
