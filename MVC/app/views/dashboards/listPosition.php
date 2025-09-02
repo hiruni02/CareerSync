7a697d9692f2579be9b1ca0130e941f01a817ebc
Anuk <anuk.thotawatta@gmail.com>
2025-09-02 11:46:16 +0530

    added some input fields for listPosition form

--- MVC/app/views/dashboards/listPosition.php
+++ MVC/app/views/dashboards/listPosition.php
@@ -3,6 +3,48 @@
     <div class="listing_application_window">
         <h1>Create a Job Position</h1>
         <button id="listing_application_backBtn">Back</button>
+        <div class="scrollbox">
+            <form method="POST" enctype="multipart/form-data">
+                <div class="input-field">
+                    <label for="posTitle">Position Title</label>
+                    <input
+                        type="text"
+                        placeholder="Position Title"
+                        name="posTitle"
+                        required
+                        value="<?= isset($_POST['posTitle']) ? htmlspecialchars($_POST['posTitle']) : '' ?>">
+                </div>
+                <div class="input-field">
+                    <label for="posType">Position Type</label>
+                    <select name="posType" id="posType">
+                        <option disabled selected hidden>Select Position Type</option>
+                        <option value="intern">Internship</option>
+                        <option value="fullTime">Full Time Employee</option>
+                        <option value="partTime">Part Time Employee</option>
+                    </select>
+                </div>
+                <div class="input-field">
+                    <label for="salaryDetails">Salary Details</label>
+                    <input
+                        type="text"
+                        placeholder="Last Name"
+                        name="salaryDetails"
+                        required
+                        value="<?= isset($_POST['salaryDetails']) ? htmlspecialchars($_POST['salaryDetails']) : '' ?>">
+                </div>
+
+                <div class="input-field">
+                    <label for="jobDescription">Job Description</label>
+                    <textarea
+                        name="jobDescription"
+                        rows="10" placeholder="Your Message"
+                        required></textarea>
+                </div>
+                <div class="form_btns">
+                    <button type="submit" class="submit">CREATE</button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
 
