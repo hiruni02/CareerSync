f49ca866ffec482a94409e9a6adc42665f8df50b
Saracodes341 <saraniya2424@gmail.com>
2025-09-14 22:59:40 +0530

    made jobdetail page

---
+++ MVC/app/views/jobdetails.view.php
@@ -0,0 +1,90 @@
+<html>
+
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= ROOT ?>assets/css/common.css">
+    <link rel="stylesheet" href="<?= ROOT ?>assets/css/jobdetails.css">
+
+        <h1 class="job_title">Outlet Manager | Colombo</h1>
+        <h3 class="company_name">ODEL PLC</h3>
+</head>
+
+<body>
+    <div class="jobinfo_box">
+        <img class="company_logo" src="C:\Users\Saraniya\Desktop\jobdetail\Odel.jpg">
+        <h4>Outlet Manager | Colombo</h4>
+        <h5>ODEL PLC</h5><br>
+
+        <p class="short_details"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-map-pin-icon lucide-map-pin">
+                <path
+                    d="M20 10c0 4.993-5.539 10.193-7.399 11.799a1 1 0 0 1-1.202 0C9.539 20.193 4 14.993 4 10a8 8 0 0 1 16 0" />
+                <circle cx="12" cy="10" r="3" />
+            </svg> Colombo</p>
+        <p class="short_details"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-clock4-icon lucide-clock-4">
+                <path d="M12 6v6l4 2" />
+                <circle cx="12" cy="12" r="10" />
+            </svg> 10 days left</p>
+        <p class="short_details"><svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24"
+                fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                class="lucide lucide-briefcase-business-icon lucide-briefcase-business">
+                <path d="M12 12h.01" />
+                <path d="M16 6V4a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v2" />
+                <path d="M22 13a18.15 18.15 0 0 1-20 0" />
+                <rect width="20" height="14" x="2" y="6" rx="2" />
+            </svg> Full time</p>
+        <hr><br>
+
+        <p class="requirement">A degree or equivalent and minimum 3 years of experience in a similar role.Please refer
+            to the job advert for
+            further information.</p>
+        <div class="job-meta">
+            <table>
+                <tr>
+                    <td>Education</td>
+                    <td><strong>BSC Degree</strong></td>
+                </tr>
+                <tr>
+                    <td>Experience</td>
+                    <td><strong>3–5 Years</strong></td>
+                </tr>
+                <tr>
+                    <td>Salary Range</td>
+                    <td><strong>Any</strong></td>
+                </tr>
+            </table>
+            <hr><br><br>
+
+            <form action="anotherPage.php" method="GET">
+                <button class="apply_job">Apply for a job</button><br>
+                <button class="save_job">Save job</button>
+            </form>
+        </div>
+    </div>
+    <div class="job_description_container">
+        <p class="job_description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Deserunt possimus sint
+            magnam exercitationem quos, tempore a ipsum blanditiis hic eligendi. Sapiente distinctio doloribus ipsam
+            modi non sequi aliquam saepe iusto, dolorum cum odio possimus esse inventore in delectus voluptatum soluta,
+            commodi deserunt adipisci laboriosam perferendis. Veritatis a impedit nam porro quos ratione dolorum, saepe
+            sapiente earum odio placeat neque sunt numquam qui quia amet possimus harum tempore? Veniam similique
+            reprehenderit animi, vel repellat fugiat quas quos deserunt. Neque officia alias blanditiis error inventore
+            natus dolorem? Consectetur porro molestiae et totam officia praesentium dolorem nemo earum libero. Impedit
+            commodi illum hic?</p>
+    </div>
+    <h2 class="position">Outlet Manager | Colombo</h2>
+
+    <p class="roles">Roles and Responsibilities</p>
+    <ul class="responsibilities">
+        <li>Managing and motivating team members to achieve performance goals</li>
+        <li>Monitoring daily operations to ensure efficiency and quality standards</li>
+        <li>Analyzing sales figures and preparing performance reports</li>
+        <li>Overseeing inventory management and stock control processes</li>
+        <li>Ensuring compliance with company policies and procedures</li>
+        <li>Building strong relationships with customers and stakeholders</li>
+    </ul>
+</body>
+
+</html>
