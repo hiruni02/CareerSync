10bfb07b622d4c4db8c3dd4273289c29004e414d
Anuk <anuk.thotawatta@gmail.com>
2025-12-15 22:00:59 +0530

    now properly fetches data for the reports

--- MVC/app/controllers/adminReport.php
+++ MVC/app/controllers/adminReport.php
@@ -1,12 +1,15 @@
 <?php
-class AdminReport{
+class AdminReport
+{
     use Controller;
     public function index()
     {
         //if not logged in the $username variable is deafulted to 'User'
         $data['username'] = empty($_SESSION['USER']) ? 'User' : $_SESSION['USER']->email;
 
+        $reportModel = new Admin();
+        $data['reportData'] = $reportModel->generateLast30DaysReport();
+
         $this->view("adminReport", $data);
     }
 }
-?>
--- MVC/app/views/adminReport.view.php
+++ MVC/app/views/adminReport.view.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <title>System Report</title>
     <link rel="stylesheet" href="<?= ROOT ?>assets/css/report.css">
 </head>
 
 <body>
-
     <h1 class="title">Monthly System Report</h1>
 
     <table class="info-table">
@@ -19,56 +19,66 @@
     </table>
 
     <h2 class="section-title">Summary</h2>
+    <?php
+    $reportDetails = $data['reportData'];
 
+    $new_companies      = $reportDetails['new_companies'] ?? 0;
+    $new_candidates     = $reportDetails['new_candidates'] ?? 0;
+    $new_counselors     = $reportDetails['new_counselors'] ?? 0;
+    $total_users        = $reportDetails['total_users'] ?? 0;
+    $active_users       = $reportDetails['active_users'] ?? 0;
+    $company_interviews = $reportDetails['company_interviews'] ?? 0;
+    $counselor_meetings = $reportDetails['counselor_meetings'] ?? 0;
+    //$feedback_emails = $reportDetails['feedback_emails'];
+    //$total_earnings = $reportDetails['total_earnings'];
+    //$sys_alerts = $reportDetails['sys_alerts'];
+    //$counselor_meetings = $reportDetails['counselor_meetings'];
+    ?>
     <table class="summary-table">
         <tr>
             <td>Newly registered companies</td>
-            <td class="amount"><?= $totalUsers ?? 69 ?></td>
+            <td class="amount"><?= $new_companies ?? 0 ?></td>
         </tr>
         <tr>
             <td>Newly registered candidates</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= $new_candidates ?? 0 ?></td>
         </tr>
         <tr>
             <td>Newly registered counselors</td>
-            <td class="amount"><?= $pendingRequests ?? 22 ?></td>
+            <td class="amount"><?= $new_counselors ?? 0 ?></td>
         </tr>
         <tr>
             <td>Total users</td>
-            <td class="amount"><?= $alerts ?? 26 ?></td>
+            <td class="amount"><?= $total_users ?? 0 ?></td>
         </tr>
         <tr>
             <td>Active users</td>
-            <td class="amount"><?= $feedback ?? 2 ?></td>
+            <td class="amount"><?= $active_users ?? 0 ?></td>
         </tr>
         <tr>
             <td>No of feedback emails</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= -1 ?></td>
         </tr>
         <tr>
             <td>Scheduled company interviews</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= $company_interviews ?? 0 ?></td>
         </tr>
         <tr>
             <td>Scheduled counselor meetings</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= $counselor_meetings ?? 0 ?></td>
         </tr>
         <tr>
             <td>total earnings in LKR</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
-        </tr>
-        <tr>
-            <td>Scheduled company interviews</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= -1 ?></td>
         </tr>
         <tr>
             <td>System alerts</td>
-            <td class="amount"><?= $activeUsers ?? 67 ?></td>
+            <td class="amount"><?= -1 ?></td>
         </tr>
     </table>
 
     <p class="footer">
-        Report generated on <?= date("Y-m-d H:i") ?>  
+        Report generated on <?= date("Y-m-d H:i") ?>
     </p>
 
 </body>
